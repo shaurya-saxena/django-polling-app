4cb1df8213aa74ac72025729327e512e1204d0ca
drinkwater <shauryasaxena.wrk@gmail.com>
2019-03-15 11:39:29 +0530

    Added custom functions to Question class. Must test.

--- polls/models.py
+++ polls/models.py
@@ -1,12 +1,26 @@
 from django.db import models
 
+from django.utils import timezone       #For custom function was_published_recently()
+import datetime                         #For custom function was_published_recently()
 # Create your models here.
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+    
+    def __str__(self):
+        return self.question_text 
+    
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+    def is_this_privacy_invasion(self):
+        return self.question_text == "Are you single" or self.question_text == "how big is your twig"
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)  
+    def __str__(self):
+        return self.choice_text
