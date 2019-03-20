4347fd73a973236a81726b48f845c9d3a0319eb2
drinkwater <shauryasaxena.wrk@gmail.com>
2019-03-20 10:57:15 +0530

    Added initial templates using render() for all views

--- polls/views.py
+++ polls/views.py
@@ -1,23 +1,38 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader
-from .models import Question
+from .models import Question, Choice
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.htm')
     context = {
         'latest_question_list': latest_question_list,
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'polls/index.htm', context)
 
 def detail(request, question_id):
-    return HttpResponse("You are looking at question %s." % question_id )
+    q = Question.objects.get(id=question_id)
+    q_text = q.question_text
+    context = {
+        'q_text': q_text
+    }
+    return render(request, 'polls/detail.htm', context)
 
 def results(request, question_id):
-    response = "You are looking at the results for question %s."
-    return HttpResponse(response % question_id)
+    q = Question.objects.get(id=question_id)
+    q_text = q.question_text
+    q_id = q.id
+    context = {
+        'q_text': q_text, 
+        'q_id': q_id,
+    }
+    return render(request, 'polls/results.htm', context)
+
 
 def vote(request, question_id):
-    response = "You're voting on question %s."
-    return HttpResponse(response % question_id)
+    q = Question.objects.get(id=question_id)
+    q_text = q.question_text
+    q_id = q.id
+    context = {
+        'q_text': q_text, 
+        'q_id': q_id,
+    }
+    return render(request, 'polls/vote.htm', context)
